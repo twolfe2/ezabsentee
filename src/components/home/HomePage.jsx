5b075f3ff0e904ddb477b2eb4485c64c4b6fc54f
John Son <jsson77@gmail.com>
2016-12-10 15:11:44 -0800

    added link to confirmation page, component live

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -5,9 +5,10 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>React Boiler</h1>
+        <h1>EZ PZ ABZNT</h1>
         <Link to="about" className="btn btn-primary">See About</Link>|
         <Link to="payment" className="btn btn-primary">See Payment</Link>|
+        <Link to="confirmation" className="btn btn-primary">CONFIRMATION</Link>|
         <Link to="dashboard" className="btn btn-primary">See Dashboard</Link>
       </div>
     );
