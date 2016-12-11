faa1471ff3c27d8748591a19039e12e414eb98a0
Alyssa Ransbury <aransbury@gmail.com>
2016-12-10 16:33:00 -0800

    Remove unnecessary code

--- src/components/confirmation/ConfirmationPage.jsx
+++ src/components/confirmation/ConfirmationPage.jsx
@@ -5,16 +5,8 @@ import SignaturePad from 'react-signature-pad';
 export default class ConfirmationPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
 
     this.isEmpty = this.isEmpty.bind(this);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleProductChange = this.handleProductChange.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleShippingChange = this.handleShippingChange.bind(this);
   }
 
   componentDidMount() {
@@ -26,23 +18,6 @@ export default class ConfirmationPage extends Component {
     const signaturePad = this.signaturePad;
     let empty = signaturePad.isEmpty();
     console.log('CDMempty: ', empty);
-  }
-
-  handleSubmit() {
-    this.setState({ is_processing: true });
-    this.paymentForm.requestCardNonce();
-  }
-
-  handleProductChange(product) {
-    this.setState({ product });
-  }
-
-  handleUserChange(user) {
-    this.setState({ user });
-  }
-
-  handleShippingChange(shipping) {
-    this.setState({ shipping });
   }
 
   render() {
@@ -63,39 +38,3 @@ export default class ConfirmationPage extends Component {
     );
   }
 }
-
-
-// class PaymentFields extends Component {
-//   render() {
-//     let cardErrorNodes = [];
-//     for (var key in this.props.cardErrors) {
-//       cardErrorNodes.push(<li key={key}>{this.props.cardErrors[key].message}</li>)
-//     }
-//     return (
-//       <div>
-//         <div id="card-errors">{cardErrorNodes}</div>
-//
-//         <div>
-//           <label>Card Number</label>
-//           <div  id="sq-card-number"></div>
-//         </div>
-//
-//         <div>
-//           <label>CVV</label>
-//           <div  id="sq-cvv"></div>
-//         </div>
-//
-//         <div>
-//           <label>Expiration Date</label>
-//           <div  id="sq-expiration-date"></div>
-//         </div>
-//
-//         <div>
-//           <label>Postal Code</label>
-//           <div  id="sq-postal-code"></div>
-//         </div>
-//
-//       </div>
-//     );
-//   }
-// }
