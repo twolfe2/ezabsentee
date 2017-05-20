3a673bd67740b315a08b96f307717a9e6f13bd67
John Son <jsson77@gmail.com>
2017-05-19 21:19:52 -0700

    deleted iconButton component

--- src/components/confirmation/components/iconButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import IconButton from 'material-ui/IconButton';
-
-const iconButton = () => (
-  <IconButton iconClassName="muidocs-icon-custom-github" />
-);
-
-export default iconButton;
